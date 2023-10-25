6a819440f1f996cce33fb781b2fefcd5fb3ee846
UmutTurk24 <umturk@davidson.edu>
2023-10-25 17:00:52 -0400

    A

--- src/Image.cs
+++ src/Image.cs
@@ -102,20 +102,16 @@ public class Image
         // Create a Bitmap object for the image
         Bitmap solution = 
             new(_width, _height, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
-
-        // Calculate the gamma correction factor
-        // var gammaFactor = 1 / _gamma;
-        var gammaFactor = _gamma;
         
         // Copy color data from the image array to the Bitmap with the Gamma Correction
         for (int i = 0; i < _width; i++) for (int j = 0; j < _height; j++) {
             var color = _image[i,j];
             
-            solution.SetPixel(i,j, Color.FromArgb(
+            solution.SetPixel(j,i, Color.FromArgb(
                 color.A,
-                (int) (Math.Pow(color.R/255.0, gammaFactor) * 255),
-                (int) (Math.Pow(color.G/255.0, gammaFactor) * 255),
-                (int) (Math.Pow(color.B/255.0, gammaFactor) * 255)
+                (int) (Math.Pow(color.R/255f, _gamma) * 255),
+                (int) (Math.Pow(color.G/255f, _gamma) * 255),
+                (int) (Math.Pow(color.B/255f, _gamma) * 255)
             ));
         }
 
