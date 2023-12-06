f6b96a5001e08b186a9b00eba2ec000d08612bac
UmutTurk24 <umturk@davidson.edu>
2023-12-06 10:28:59 -0500

    arrangements

--- src/HW5Controller.cs
+++ src/HW5Controller.cs
@@ -15,32 +15,12 @@ public class HW5Controller
 {
     public static void Main()
     {
-
-        // Camera c2 = new Camera(Camera.Projection.Perspective,
-        // new Vector(0.0f, 20.0f, 80.0f),
-        // new Vector(0.0f, 0f, 0f),
-        // new Vector(0.0f, 1f, 0f),
-        // 0.1f, 150f, 512, 512, -10f, 10f, -10f, 10f);
-
-        // //Build the scene
-        // Scene scene2 = new Scene();
-        // scene2.Light = new Vector(-30.0f,100.0f, 80.0f);
-        // Shape s1 = new Sphere(new Vector(0.0f, 20.0f, 50.0f), 20f);
-        // Shape s2 = new Sphere(new Vector(-50.0f, 25.0f, 40.0f), 20f);
-        // Shape s3 = new Sphere(new Vector(60f, 40f, -10.0f), 70f);
-
-        // s1.D = new Vector(0, 0, 255f);
-        // s2.D = new Vector(0, 255f, 0f);
-        // s3.D = new Vector(255f, 0f, 0f);
-
-        // Shape p2 = new Plane();
-        // scene2.AddShape(ref p2);
-        // scene2.AddShape(ref s3);
-        // scene2.AddShape(ref s2);
-        // scene2.AddShape(ref s1);
-
-        // // c2.RenderImageParallel("SphereScene2.bmp", scene2, 10);
-        // c2.RenderSqImageParallel("Sphere-7.bmp", scene2, 10, 7);
+        /// Do tests for 
+        /// SqSphere-6
+        /// Origin-001-001
+        /// Direction-5-5
+        /// GSphere-3-9
+        /// SSphere-10-500
 
 
         // Orthographic Test
